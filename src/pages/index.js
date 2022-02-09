393db61f03a350fffa01a2c96b36449afa2f7a5c
AdriJ93 <adrianjerome.wulfu@gmail.com>
2022-02-09 08:01:16 +0100

    v2.05

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,26 @@ import "../styles/Home.css"
 import Title from "../images/EditorDesktopMatt.jpg"
 import { Link } from "gatsby"
 import Layout from "../components/Layout.js"
+import { Helmet, helmet } from "react-helmet"
 
 function Home() {
   return (
-    <Layout>
-      <div className="home" style={{ backgroundImage: `url(${Title})` }}>
-        <div className="container">
-          <h1> Cutter oder Video Editor Freelancer gesucht?</h1>
-          <p> Unkompliziert und schnell Videoschnitt Projekte umsetzen!</p>
-          <Link to="/offers">
-            <button className="button"> Unsere Angebote </button>
-          </Link>
+    <>
+      <Helmet>
+        <title>CREATE NOW</title>
+      </Helmet>
+      <Layout>
+        <div className="home" style={{ backgroundImage: `url(${Title})` }}>
+          <div className="container">
+            <h1> Cutter oder Video Editor Freelancer gesucht?</h1>
+            <p> Unkompliziert und schnell Videoschnitt Projekte umsetzen!</p>
+            <Link to="/offers">
+              <button className="button"> Unsere Angebote </button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
 
