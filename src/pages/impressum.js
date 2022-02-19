0163cad336ac14f56f46fa5f557a3970d0c9207a
AdriJ93 <adrianjerome.wulfu@gmail.com>
2022-02-19 10:09:33 +0100

    v6532

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -1,39 +1,50 @@
 import React from "react"
 import "../styles/Impressum.css"
 import Layout from "../components/Layout.js"
+import TitleWhite from "../images/whiteBackground.jpg"
 
 function Impressum() {
   return (
     <Layout>
-      <div className="impressum">
-        <h1>Impressum</h1>
-        <h2>Angaben gemäß § 5 TMG:</h2>
-        <p>
-          Adrian Wulf
-          <br />
-          Einsteinstraße 104
-          <br />
-          81675 München
-        </p>
-        <h2>Streitschlichtung:</h2>
-        <p>
-          Die Europäische Kommission stellt eine Plattform zur
-          Online-Streitbeilegung (OS) bereit:
-          https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home.chooseLanguage
-          Unsere E-Mail Adressen finden sie im Impressum. Wir sind nicht bereit
-          oder verpflichtet, an Steigbeilegungsverfahren vor einer
-          Verbraucherschlichtungsstelle teilzunehmen.
-        </p>
-        <h2>Kontakt:</h2>
-        <p>E-Mail: adrian.wulf@medimeister24.de</p>
-        <h2>Redaktionell verantwortlich:</h2>
-        <p>
-          Adrian Wulf
-          <br />
-          Einsteinstraße 104
-          <br />
-          81675 München
-        </p>
+      <div
+        className="impressum"
+        style={{ backgroundImage: `url(${TitleWhite})` }}
+      >
+        <div className="impressum-container">
+          <h1>Impressum</h1>
+          <h2>Angaben gemäß § 5 TMG:</h2>
+          <p>
+            Adrian Wulf
+            <br />
+            Einsteinstraße 104
+            <br />
+            81675 München
+          </p>
+          <h2>Streitschlichtung:</h2>
+          <p>
+            Die Europäische Kommission stellt eine Plattform zur
+            <a
+              className="impressum-link"
+              href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home.chooseLanguage"
+            >
+              {" "}
+              Online-Streitbeilegung (OS){" "}
+            </a>
+            bereit. Unsere E-Mail Adressen finden sie im Impressum. Wir sind
+            nicht bereit oder verpflichtet, an Steigbeilegungsverfahren vor
+            einer Verbraucherschlichtungsstelle teilzunehmen.
+          </p>
+          <h2>Kontakt:</h2>
+          <p>E-Mail: info@create-now.net</p>
+          <h2>Redaktionell verantwortlich:</h2>
+          <p>
+            Adrian Wulf
+            <br />
+            Einsteinstraße 104
+            <br />
+            81675 München
+          </p>
+        </div>
       </div>
     </Layout>
   )
